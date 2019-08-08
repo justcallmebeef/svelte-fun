0a9bd73c9f7452a14e830964c2ae7d314a5e9e95
Bethany Mitch <bmitch@AS-0065.local>
2019-08-08 13:22:00 -0600

    messing around with props and functions

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,21 @@
 <script>
-	let name = 'Svelte!!';
+	let name = 'Svelte!!'
+
+	function nameChange() {
+		console.log(name)
+		name = 'test function stuff'
+		console.log(name)
+	}
 </script>
 
 <style>
-h1 {
-	color: purple; 
-}
+
 </style>
 
-<h1>Hello {name}!</h1>
+<h1 color="primary">Hello {name}!</h1>
+
+<form>
+Name: <br>
+<input type="text">
+<button on:click={nameChange}>Say Hello to me!</button>
+</form>
